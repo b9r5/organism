76a8e64f98c35461eac6aef7c04ec7451e46d702
Ben Horowitz <bhorowit@gmail.com>
2024-07-10 07:06:48 -0700

    Split data into training and validation

--- create_training_data.py
+++ create_training_data.py
@@ -1,11 +1,28 @@
-with open('data/all_dinosaurs.txt', 'r', encoding='utf-8') as f:
-    text = f.read()
+import random
 
-lines = text.splitlines()
+with open('data/all_dinosaurs.txt', 'r') as f:
+    lines = f.readlines()
 
-with open('data/dinosaurs_for_training.csv', 'w', encoding='utf-8') as f:
-    for line in lines:
-        for i in range(len(line)):
-            prefix = line[:i+1]
-            char = "#" if i+1 == len(line) else line[i+1:i+2]
-            f.write(f"{prefix},{char}\n")
+random.seed(42)
+
+random.shuffle(lines)
+
+with open('data/shuffled_dinosaurs.txt', 'w') as f:
+    f.writelines(lines)
+
+split = 0.9
+
+def line_to_xy(line):
+    """Given a line, returns the x,y training data for the line."""
+    line = line.strip()
+    x = line
+    y = f'{line[1:]}$' # $ signifies end of dinosaur name
+    return f'{x},{y}\n'
+
+with open('data/training_dinosaurs.csv', 'w', encoding='utf-8') as f:
+    for i in range (int(0.9 * len(lines))):
+        f.write(line_to_xy(lines[i]))
+
+with open('data/validation_dinosaurs.csv', 'w', encoding='utf-8') as f:
+    for i in range(int(0.9 * len(lines)), len(lines)):
+        f.write(line_to_xy(lines[i]))
